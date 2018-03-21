101a0431aea1006203552948c64b41b4a4688f4d
Dias <standlight@@yandex.ru>
2018-03-21 17:28:11 +0300

    Ready first screen

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,19 @@ import FullPage from 'fullpage.js';
 var $ = require("jquery");
 
 $(document).ready(function (e) {
-    console.log('asdasdas');
-	$('#full-page-wrapper').fullpage();
+    $('#full-page-wrapper').fullpage({
+        menu: '#scroll-nav',
+        anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
+        onLeave: function (anchorLink, index) {
+            $('#scroll-nav .slide-name').fadeOut(300);
+        },
+        afterLoad: function (anchorLink, index) {
+            console.log(1);
+            var name = $('#' + anchorLink + '-menu-title').attr('data-rus-name');
+            console.log(2);
+            $('#scroll-nav .slide-name').html(name);
+            $('#scroll-nav .slide-name').fadeIn(300);
+            console.log(3);
+        }
+    });
 })
