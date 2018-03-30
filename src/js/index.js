d91a4efaab3d875f53b43fdc5b4314dd49be7262
Dias <standlight@@yandex.ru>
2018-03-30 13:44:22 +0300

    Какие то правки внес

--- src/js/index.js
+++ src/js/index.js
@@ -3,20 +3,29 @@ import FullPage from 'fullpage.js';
 var $ = require("jquery");
 
 $(document).ready(function (e) {
+    $('#slide1 .anchor').click(function (e) {
+        console.log($(this).attr('data-item'));
+        console.log($($(this).attr('data-item')));
+        $($(this).attr('data-item')).click();
+        $.fn.fullpage.moveTo(3);
+    });
     $('#full-page-wrapper').fullpage({
         menu: '#scroll-nav',
-        anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
+        anchors: ['firstPage', 'secondPage', '3rdPage', '4thPage', '5thPage'],
         onLeave: function (anchorLink, index) {
             $('#scroll-nav .slide-name').fadeOut(300);
         },
         afterLoad: function (anchorLink, index) {
-            console.log(1);
             var name = $('#' + anchorLink + '-menu-title').attr('data-rus-name');
-            console.log(2);
             $('#scroll-nav .slide-name').html(name);
             $('#scroll-nav .slide-name').fadeIn(300);
-            console.log(3);
         }
     });
-
+    $("#slide3 .parent-list .item").click(function (e) {
+        $("#slide3 .parent-list .item").removeClass('active');
+        $("#slide3 .child-list").removeClass('active');
+        $(this).addClass('active');
+        var selector = $(this).attr('data-item');
+        $("#"+selector).addClass('active');
+    })
 });
