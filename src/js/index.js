28016d7acdce0cdf5a9d63ffe9d7ac67c81461e7
Dias <standlight@@yandex.ru>
2018-04-03 01:22:31 +0300

    Some changes before uploading to github

--- src/js/index.js
+++ src/js/index.js
@@ -11,7 +11,7 @@ $(document).ready(function (e) {
     });
     $('#full-page-wrapper').fullpage({
         menu: '#scroll-nav',
-        anchors: ['firstPage', 'secondPage', '3rdPage', '4thPage', '5thPage'],
+        anchors: ['firstPage', 'secondPage', '3rdPage', '4thPage', '5thPage', '6thPage'],
         onLeave: function (anchorLink, index) {
             $('#scroll-nav .slide-name').fadeOut(300);
         },
@@ -21,11 +21,18 @@ $(document).ready(function (e) {
             $('#scroll-nav .slide-name').fadeIn(300);
         }
     });
+
+    $(".fp-tableCell").css('height', '100%');
+    $('.fp-next').html("<i class='icon-right'></i>")
     $("#slide3 .parent-list .item").click(function (e) {
         $("#slide3 .parent-list .item").removeClass('active');
         $("#slide3 .child-list").removeClass('active');
         $(this).addClass('active');
         var selector = $(this).attr('data-item');
         $("#"+selector).addClass('active');
+    })
+
+    $('.footer .bottom-button').click(function () {
+        $.fn.fullpage.moveSectionDown();
     })
 });
