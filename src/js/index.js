ae82ead085cae7b5e76ebc40359b4687dbaea36f
Dias <standlight@@yandex.ru>
2018-04-04 01:43:11 +0300

    Финишная прямая

--- src/js/index.js
+++ src/js/index.js
@@ -4,11 +4,13 @@ var $ = require("jquery");
 
 $(document).ready(function (e) {
     $('#slide1 .anchor').click(function (e) {
-        console.log($(this).attr('data-item'));
-        console.log($($(this).attr('data-item')));
-        $($(this).attr('data-item')).click();
         $.fn.fullpage.moveTo(3);
     });
+    $(".to-slide").click(function (e) {
+        e.preventDefault();
+        var slide = $(this).attr("data-slide");
+        $.fn.fullpage.moveTo(slide);
+    })
     $('#full-page-wrapper').fullpage({
         menu: '#scroll-nav',
         anchors: ['firstPage', 'secondPage', '3rdPage', '4thPage', '5thPage', '6thPage','7thPage'],
@@ -23,15 +25,7 @@ $(document).ready(function (e) {
     });
 
     $(".fp-tableCell").css('height', '100%');
-    $('.fp-next').html("<i class='icon-right'></i>")
-    $("#slide3 .parent-list .item").click(function (e) {
-        $("#slide3 .parent-list .item").removeClass('active');
-        $("#slide3 .child-list").removeClass('active');
-        $(this).addClass('active');
-        var selector = $(this).attr('data-item');
-        $("#"+selector).addClass('active');
-    })
-
+    $('.fp-next').html("<i class='icon-right'></i>");
     $('.footer .bottom-button').click(function () {
         $.fn.fullpage.moveSectionDown();
     })
